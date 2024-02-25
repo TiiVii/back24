bc00f171b9eb323506f66021548380f4864bafd2
TiiVii <111729213+TiiVii@users.noreply.github.com>
2024-02-25 13:10:37 +0200

    Update entry-controller.mjs

--- src/controllers/entry-controller.mjs
+++ src/controllers/entry-controller.mjs
@@ -1,7 +1,15 @@
-import {listAllEntries, findEntryById, addEntry, updateEntryById, deleteEntryById} from "../models/entry-model.mjs";
+import {
+  findEntryById,
+  addEntry,
+  deleteEntryById,
+  updateEntryById,
+  listAllEntriesByUserId,
+} from '../models/entry-model.mjs';
 
 const getEntries = async (req, res) => {
-  const result = await listAllEntries();
+  // return only logged in user's own entries
+  // - get user's id from token (req.user.user_id)
+  const result = await listAllEntriesByUserId(req.user.user_id);
   if (!result.error) {
     res.json(result);
   } else {
@@ -36,34 +44,26 @@ const postEntry = async (req, res) => {
 };
 
 const putEntry = async (req, res) => {
-  const { id } = req.params;
-  const { user_id, entry_date, mood, weight, sleep_hours, notes } = req.body;
-  if (id && (entry_date || mood || weight || sleep_hours || notes || user_id)) {
-    const result = await updateEntryById(id, req.body);
-    if (!result.error) {
-      res.sendStatus(204); // No Content
-    } else {
-      res.status(500);
-      res.json(result);
+  const entry_id = req.params.id;
+  const {entry_date, mood, weight, sleep_hours, notes} = req.body;
+  // check that all needed fields are included in request
+  if ((entry_date || weight || mood || sleep_hours || notes) && entry_id) {
+    const result = await updateEntryById({entry_id, ...req.body});
+    if (result.error) {
+      return res.status(result.error).json(result);
     }
+    return res.status(201).json(result);
   } else {
-    res.sendStatus(400);
+    return res.status(400).json({error: 400, message: 'bad request'});
   }
 };
 
 const deleteEntry = async (req, res) => {
-  const { id } = req.params;
-  if (id) {
-    const result = await deleteEntryById(id);
-    if (!result.error) {
-      res.sendStatus(204); // No Content
-    } else {
-      res.status(500);
-      res.json(result);
-    }
-  } else {
-    res.sendStatus(400);
+  const result = await deleteEntryById(req.params.id);
+  if (result.error) {
+    return res.status(result.error).json(result);
   }
+  return res.json(result);
 };
 
 export {getEntries, getEntryById, postEntry, putEntry, deleteEntry};
