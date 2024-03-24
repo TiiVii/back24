298bfdd60ea975042526f800a53dac8309986344
TiiVii <111729213+TiiVii@users.noreply.github.com>
2024-03-24 18:17:54 +0200

    hh

--- db/create-db-user-example.sql
+++ db/create-db-user-example.sql
@@ -1,3 +1,3 @@
-CREATE USER 'myusername'@'localhost' IDENTIFIED BY 'mypassword';
-GRANT ALL PRIVILEGES ON `databasename`.* TO 'myusername'@'localhost';
+CREATE USER 'TeeVee'@'localhost' IDENTIFIED BY 'vaahtokarkki';
+GRANT ALL PRIVILEGES ON `HealthDiary`.* TO 'TeeVee'@'localhost';
 FLUSH PRIVILEGES;
